1b538f9eeb32b744f9c9c03fd108942b99979584
wataru sato <watarusato@thebes.(none)>
2012-02-24 11:47:34 +0900

    resizableとdraggableを適用

--- app/assets/javascripts/announcements.js
+++ app/assets/javascripts/announcements.js
@@ -3,3 +3,74 @@ $(document).ready(function(){
     var offset = $("div#wrong_picture").offset();
     p.html( "left: " + offset.left + ", top: " + offset.top );
   });
+  
+  $(function(){
+    $(".point_1").resizable({
+      handles: "all",
+      containment: "#wrong_picture",
+      resize: function(event, ui){
+        $("point_1").css('z-index', '99999');
+      }
+    }).draggable({
+      containment: "#wrong_picture",
+      create: function(event, ui){
+        $(".point_1").css("z-index", "99999");
+      }
+    })
+  });
+  $(function(){
+    $(".point_2").resizable({
+      handles: "all",
+      containment: "#wrong_picture",
+      resize: function(event, ui){
+        $("point_2").css('z-index', '99999');
+      }
+    }).draggable({
+      containment: "#wrong_picture",
+      create: function(event, ui){
+        $(".point_2").css("z-index", "99999");
+      }
+    })
+  });
+  $(function(){
+    $(".point_3").resizable({
+      handles: "all",
+      containment: "#wrong_picture",
+      resize: function(event, ui){
+        $("point_3").css('z-index', '99999');
+      }
+    }).draggable({
+      containment: "#wrong_picture",
+      create: function(event, ui){
+        $(".point_3").css("z-index", "99999");
+      }
+    })
+  });
+  $(function(){
+    $(".point_4").resizable({
+      handles: "all",
+      containment: "#wrong_picture",
+      resize: function(event, ui){
+        $("point_4").css('z-index', '99999');
+      }
+    }).draggable({
+      containment: "#wrong_picture",
+      create: function(event, ui){
+        $(".point_4").css("z-index", "99999");
+      }
+    })
+  });
+  $(function(){
+    $(".point_5").resizable({
+      handles: "all",
+      containment: "#wrong_picture",
+      resize: function(event, ui){
+        $("point_5").css('z-index', '99999');
+      }
+    }).draggable({
+      containment: "#wrong_picture",
+      create: function(event, ui){
+        $(".point_5").css("z-index", "99999");
+      }
+    })
+  });
