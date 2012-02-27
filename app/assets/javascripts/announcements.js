c4a04b7d5f6f39d9f5e70d8b7c7c6d3614dcfbf5
wataru sato <watarusato@thebes.(none)>
2012-02-27 16:00:07 +0900

    高さと座標の表示まで

--- app/assets/javascripts/announcements.js
+++ app/assets/javascripts/announcements.js
@@ -1,34 +1,39 @@
-$(document).ready(function(){
-    var p = $("p.out_put");
-    var offset = $("div#wrong_picture").offset();
-    p.html( "left: " + offset.left + ", top: " + offset.top );
-  });
-  
-  $(function(){
-    $(".point_1").resizable({
-      handles: "all",
-      containment: "#wrong_picture",
-      resize: function(event, ui){
-        $("point_1").css('z-index', '99999');
-      }
+$(function(){
+  $(".point_1").resizable({
+    handles: "all",
+    maxWidth: 200,
+    maxHeight: 200,
+    resize: function(event, ui){
+      $("point_1").css('z-index', '99999');
+      $("point_1").css("margin", "0");
+      var P1W = $(".point_1").css("width");
+      var P1H = $(".point_1").css("height");
+      $(".point_1_out").empty().prepend(P1W);
+      $(".point_2_out").empty().prepend(P1H);
+    }
     }).draggable({
       containment: "#wrong_picture",
-      create: function(event, ui){
+      drag: function(event, ui){
         $(".point_1").css("z-index", "99999");
+        $(".point_1").css("margin", "0");
+        var span = $("span.out_put_point_1");
+        var P1 = $(".point_1").position();
+        span.html( "left: " + Math.floor( P1.left - 60 ) + ", top: " + Math.floor( P1.top - 282 ) );
       }
     })
   });
+ 
   $(function(){
     $(".point_2").resizable({
       handles: "all",
       containment: "#wrong_picture",
       resize: function(event, ui){
-        $("point_2").css('z-index', '99999');
+        $("point_2").css('z-index', '99998');
       }
     }).draggable({
       containment: "#wrong_picture",
       create: function(event, ui){
-        $(".point_2").css("z-index", "99999");
+        $(".point_2").css("z-index", "99998");
       }
     })
   });
@@ -37,12 +42,12 @@ $(document).ready(function(){
       handles: "all",
       containment: "#wrong_picture",
       resize: function(event, ui){
-        $("point_3").css('z-index', '99999');
+        $("point_3").css('z-index', '99997');
       }
     }).draggable({
       containment: "#wrong_picture",
       create: function(event, ui){
-        $(".point_3").css("z-index", "99999");
+        $(".point_3").css("z-index", "99997");
       }
     })
   });
@@ -51,12 +56,12 @@ $(document).ready(function(){
       handles: "all",
       containment: "#wrong_picture",
       resize: function(event, ui){
-        $("point_4").css('z-index', '99999');
+        $("point_4").css('z-index', '99996');
       }
     }).draggable({
       containment: "#wrong_picture",
       create: function(event, ui){
-        $(".point_4").css("z-index", "99999");
+        $(".point_4").css("z-index", "99996");
       }
     })
   });
@@ -65,12 +70,12 @@ $(document).ready(function(){
       handles: "all",
       containment: "#wrong_picture",
       resize: function(event, ui){
-        $("point_5").css('z-index', '99999');
+        $("point_5").css('z-index', '99995');
       }
     }).draggable({
       containment: "#wrong_picture",
       create: function(event, ui){
-        $(".point_5").css("z-index", "99999");
+        $(".point_5").css("z-index", "99995");
       }
     })
   });
